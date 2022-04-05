224d67ba5c7cdbbb7ff2f59bbbd271bb1c5d36fe
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-04-05 16:55:53 -0500

    commit tree c07f08728c5fca0c6512c41da0992a92fb471541

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function saturdayFun(action = 'roller-skate'){return `This Saturday, I want to ${action}!`}
+
+function mondayWork(action = "go to the office") {return `This Monday, I will ${action}.` }
+
+function wrapAdjective(flair){
+    return function(adjective = "special"){ return 'You are ' + flair + adjective + flair + '!'}
+}
+
+console.log(wrapAdjective('*')())
